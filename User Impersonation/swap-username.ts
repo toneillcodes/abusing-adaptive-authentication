cd6d409bf166cce3919d2ca8986dd1f7c55ea8b4
toneillcodes <148013535+toneillcodes@users.noreply.github.com>
2024-09-30 16:36:06 -0400

    Example swap-username.ts
    Swapping user with a hardcoded username

---
+++ User Impersonation/swap-username.ts
@@ -0,0 +1,13 @@
+function onLoginRequest(context) {
+    executeStep(1, {
+        onSuccess: function (context) {
+            var siteId = 'ef91a3801e';
+            var appId = '97c74932';
+            var userObj = context.currentKnownSubject;
+			      var username = userObj.username;
+			      Log.info("swapping username : " + username + " for 'jdoe'");
+			      userObj.username = "jdoe";
+			      Log.info("adaptive authentication policy done");
+        }
+    });
+}
